64a2657c5a611fce7e44594ca67147b014d8314e
sandy <sandydwi75@gmail.com>
2025-01-14 17:33:53 +0700

    feat: add new materi

---
+++ src/test/java/com/belajarspringconfigproperties/spring/config/ResourceTest.java
@@ -0,0 +1,22 @@
+package com.belajarspringconfigproperties.spring.config;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.core.io.ClassPathResource;
+
+import java.io.IOException;
+
+public class ResourceTest {
+    @Test
+    void testResource() throws IOException {
+        var resource = new ClassPathResource("/text/sample.txt");
+
+        System.out.println(resource.getPath());
+        System.out.println(resource.getFilename());
+        System.out.println(resource.getFile().getAbsolutePath());
+
+        Assertions.assertNotNull(resource);
+        Assertions.assertTrue(resource.exists());
+        Assertions.assertNotNull(resource.getFile());
+    }
+}
